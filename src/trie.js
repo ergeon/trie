fc869453e7b82bcc509ce0b3fd601b004f2d9d3c
Ivan Diaz <ivan@opensupports.com>
2020-04-26 05:40:30 -0300

    Add array support for trie

--- src/trie.js
+++ src/trie.js
@@ -11,24 +11,35 @@ const TrieNode = require('./trieNode');
  */
 class Trie {
   constructor() {
-    this._root = new TrieNode('');
+    this._root = new TrieNode(null);
     this._wordsCount = 0;
     this._nodesCount = 1; // root node
   }
 
   /**
+   * @private
+   * returns word as an Array
+   * @param {string|Array} word
+   * @returns {Array}
+   */
+  static getWordAsArray(word) {
+    return Array.isArray(word) ? word : Array.from(word);
+  }
+
+  /**
    * @public
    * inserts a word into the trie and returns its last char node
-   * @param {string} word
+   * @param {string|Array} _word
    * @param {TrieNode} node
    * @param {number} i
    * @returns {TrieNode}
    */
-  insert(word, node = this._root, i = 0) {
-    if (typeof word !== 'string') {
-      throw new Error('Trie.insert expects a string word');
+  insert(_word, node = this._root, i = 0) {
+    if (typeof _word !== 'string' && !Array.isArray(_word)) {
+      throw new Error('Trie.insert expects a string or array as word');
     }
 
+    const word = Trie.getWordAsArray(_word);
     if (i === word.length) {
       if (!node.isEndOfWord()) {
         node.setEndOfWord(true);
@@ -48,13 +59,14 @@ class Trie {
   /**
    * @public
    * checks if a word exists in the trie
-   * @param {string} word
+   * @param {string|Array} _word
    * @param {TrieNode} node
    * @param {number} i
    * @returns {boolean}
    */
-  has(word, node = this._root, i = 0) {
-    if (typeof word !== 'string') return false;
+  has(_word, node = this._root, i = 0) {
+    if (typeof _word !== 'string' && !Array.isArray(_word)) return false;
+    const word = Trie.getWordAsArray(_word);
 
     if (i === word.length) {
       return node.isEndOfWord();
@@ -68,13 +80,14 @@ class Trie {
   /**
    * @public
    * finds a word in the trie and returns its last char node
-   * @param {string} word
+   * @param {string|Array} _word
    * @param {TrieNode} node
    * @param {number} i
    * @returns {TrieNode}
    */
-  find(word, node = this._root, i = 0) {
-    if (typeof word !== 'string') return null;
+  find(_word, node = this._root, i = 0) {
+    if (typeof _word !== 'string' && !Array.isArray(_word)) return null;
+    const word = Trie.getWordAsArray(_word);
 
     if (i === word.length) {
       return node.isEndOfWord() ? node : null;
@@ -88,22 +101,23 @@ class Trie {
   /**
    * @public
    * removes a word from the trie
-   * @param {string} word
+   * @param {string|Array} _word
    * @returns {boolean}
    */
-  remove(word) {
-    const lastCharNode = this.find(word);
+  remove(_word) {
+    const word = Trie.getWordAsArray(_word);
+    const lastNode = this.find(word);
 
-    if (lastCharNode === null) return false;
+    if (lastNode === null) return false;
 
-    if (lastCharNode.childrenCount() > 0 || word === '') {
-      lastCharNode.setEndOfWord(false);
+    if (lastNode.childrenCount() > 0 || !word.length) {
+      lastNode.setEndOfWord(false);
       this._wordsCount -= 1;
       return true;
     }
 
-    let current = lastCharNode;
-    while (current.getChar() !== '') {
+    let current = lastNode;
+    while (current.getChar() !== null) {
       if (current.childrenCount() === 0) {
         current.getParent().removeChild(current.getChar());
         this._nodesCount -= 1;
@@ -120,23 +134,82 @@ class Trie {
    * traverse the words in the trie
    * @param {function} cb
    * @param {TrieNode} node
-   * @param {string} w
+   * @param {string|Array} w
    */
-  forEach(cb, node = this._root, w = '') {
+  forEach(cb, node = this._root, w = []) {
     if (typeof cb !== 'function') {
       throw new Error('Trie.forEach expects a callback');
     }
 
-    let word = w;
+    let word = Trie.getWordAsArray(w);
     if (node.isEndOfWord()) {
-      cb(word);
+      cb(word.every((item) => typeof item === 'string') ? word.join('') : word);
     }
 
     node.children().forEach((child) => {
-      word += child.getChar();
+      word = [...word, child.getChar()];
       this.forEach(cb, child, word); // depth-first search
-      word = word.substr(0, word.length - 1); // backward tracking
+      word = word.slice(0, word.length - 1); // backward tracking
     });
+  }
+
+  /**
+   * @public
+   * returns word that matches longest prefix with your input
+   * @param {string|Array} _word
+   * @param {TrieNode} node
+   * @param {number} i
+   * @returns {Array}
+   */
+  findClosestWord(_word, node = this._root, i = 0) {
+    if (typeof _word !== 'string' && !Array.isArray(_word)) return null;
+    const word = Trie.getWordAsArray(_word);
+    let child;
+
+    if (!node.getChildren().size) return [node.getChar()];
+
+    if (i === word.length) {
+      return [node.getChar()];
+    }
+
+    child = word.find((item) => node.getChildren().has(item));
+    if (!child) {
+      [child] = Array.from(node.getChildren().values());
+    } else {
+      child = node.getChildren().get(child);
+    }
+
+    return node.getChar() ? [
+      node.getChar(),
+      ...this.findClosestWord(word, child, i + 1)
+    ] : this.findClosestWord(word, child, i + 1);
+  }
+
+  /**
+   * @public
+   * returns possible options for each item in input word
+   * @param {string|Array} _word
+   * @param {TrieNode} node
+   * @param {number} i
+   * @returns {Array}
+   */
+  getOptions(_word, node = this._root, i = 0) {
+    if (typeof _word !== 'string' && !Array.isArray(_word)) return null;
+    const word = Trie.getWordAsArray(_word);
+    let child;
+
+    if (!node.getChildren().size) return [];
+
+    if (!node.hasChild(word[i])) {
+      [child] = Array.from(node.getChildren().values());
+    } else {
+      child = node.getChild(word[i]);
+    }
+
+    return [
+      Array.from(node.getChildren().keys()),
+      ...this.getOptions(word, child, i + 1)
+    ];
   }
 
   /**
@@ -171,7 +244,7 @@ class Trie {
    * clears the trie
    */
   clear() {
-    this._root = new TrieNode('');
+    this._root = new TrieNode(null);
     this._nodesCount = 1;
     this._wordsCount = 0;
   }
